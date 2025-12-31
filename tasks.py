559aba1727678eaa5abb96ed0d80ed4e5f38f67d
KORMA-SAAD <2005073000205@ofppt-edu.ma>
2025-12-31 11:58:56 +0100

    add print(logging task)

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 tasks = []
 def add_task(task):
     tasks.append(task)
-    print("Task added : ",task)
+    print("Task added : ",task)if not task : print('Empty task not allwed') return 
+print("Logging task...")
